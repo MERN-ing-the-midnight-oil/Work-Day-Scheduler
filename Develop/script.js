48080948d127dfea1c3e0ec2f9527946ddc7bbef
Rhys Smoker <r.smoker@gmail.com>
2022-10-29 18:53:17 -0700

    minor formatting cahnges

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,16 @@
-//pass the textarea value from memory to the element I want
-$("#nineTextarea").val(localStorage.getItem("nineTextarea")); // make one of these for each hour to get persisted memory
-console.log($("#nineTextarea")); //note that with "val" js can go both ways, either putting IN or taking OUT
+//I want to add a class to a timeblock when a condition is met
+//specifically when the current time is greater than the div's blockstart time, then get .past class
+
+//pass the textarea value from memory to the value of the element
+$("#nineTextarea").val(localStorage.getItem("nineTextarea")); //gives the item in local storage to the val of #nineTextarea //
+$("#tenTextarea").val(localStorage.getItem("tenTextarea"));
+$("#elevenTextarea").val(localStorage.getItem("elevenTextarea"));
+$("#twelveTextarea").val(localStorage.getItem("twelveTextarea"));
+$("#oneTextarea").val(localStorage.getItem("oneTextarea"));
+$("#twoTextarea").val(localStorage.getItem("twoTextarea"));
+$("#threeTextarea").val(localStorage.getItem("threeTextarea"));
+$("#fourTextarea").val(localStorage.getItem("fourTextarea"));
+$("#fiveTextarea").val(localStorage.getItem("fiveTextarea"));
 
 $(".saveBtn").on("click", function () {
 	//event listener , listening for clicks, on any save button
@@ -11,26 +21,24 @@ $(".saveBtn").on("click", function () {
 	// key =(Btn to textarea to textarea ID)
 });
 
+//examples of removing and adding a class (from a different project)
+//gameOver.classList.remove("hidden");
+//	timerEl.classList.add("hidden");
+//.hidden {display:none} in CSS     class.classlist.add"hidden"
+
 /////////////////////
 //TIME STUFF
 ////////////////
-
 var todayIs = moment();
 $("#todayIs").text(todayIs.format("MMM Do, YYYY"));
-
-//We are going to need the current time , but this needs to either convert to hours only or the westCoast var needs to convert to something comparable to this var
-
 var timeNow = moment().format("hh:mm:ss"); //this needs to udate constantly somehow , not just on load
 $("#timeNow").text(timeNow);
 
 //converts epoch time to the military hour of the day (west coast time)
 var epochTimeNow = moment().format("X");
-
 var secondsRemainder = epochTimeNow % 86400; // used modulus, should be the number of seconds happening since after our last midnight
-
 var hoursRemainder = secondsRemainder / 3600; //shoud be the military hour, GMT
-
-var westCoast = hoursRemainder - 7; //subtracts 7 from GMT to get pacific time zone time, could improve this later
+var westCoast = hoursRemainder - 7; //subtracts 7 from GMT to get pacific time zone time
 
 // WHEN I open the planner
 // THEN the current day is displayed at the top of the calendar
