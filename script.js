a43e0ced8fce586524774c93e690f374224401e0
Rhys Smoker <r.smoker@gmail.com>
2022-10-31 21:46:01 -0700

    took files out of the develop folder and deleted the develop folder so github will actually deploy for real

--- script.js
+++ script.js
@@ -48,26 +48,8 @@ for (let i = 0; i < timeBlocksEl.length; i++) {
 	}
 }
 
-// WHEN I open the planner
-// THEN the current day is displayed at the top of the calendar
-
-// WHEN I scroll down
-// THEN I am presented with timeblocks for standard business hours
-
-// WHEN I view the timeblocks for that day
-// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-//I think I'm going to need a test- IF a block is in the past, style past, else if style present, else future.  But how to determine if a block is in the future? Not simple. First find if it is in the present, should be easier. Maybe first
-
-// WHEN I click into a timeblock
-// THEN I can enter an event
-
-// WHEN I click the save button for that timeblock
-// THEN the text for that event is saved in local storage
-
-// WHEN I refresh the page
-// THEN the saved events persist
-
-//This  function displays the current time, "westCoast" in the Jumbotron
+//This  function displays the current time, "westCoast" in the Jumbotron. Its an overly complicated way
+//to get the current time in hours.
 // it converts epoch time to the military hour of the day (west coast time). epoch time is set to GMT, 7 hours ahead.
 // I had to figure out when to subract 7 hours, and when to add 17. You can't subtract 7 hours early in the day or you get a negative time value.
 // var epochTimeNow = moment().format("X");
